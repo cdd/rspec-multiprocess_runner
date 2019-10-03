79cb6a73f116ead556fd81c56b25918a86cfff05
Jacob Bloom <jacob@collaborativedrug.com>
2019-10-03 10:45:31 -0700

    Post release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+# 1.3.2
+
 # 1.3.1
 
 * Or use an environment variable for summary file.
--- lib/rspec/multiprocess_runner/version.rb
+++ lib/rspec/multiprocess_runner/version.rb
@@ -1,5 +1,5 @@
 module RSpec
   module MultiprocessRunner
-    VERSION = "1.3.1"
+    VERSION = "1.3.2.pre"
   end
 end
